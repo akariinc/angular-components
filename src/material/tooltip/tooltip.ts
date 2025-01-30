907fa560782c95225e93d6ebfd50e3a6a3d03005
Yosuke Yamashita <yosuke@aka.jp>
2025-01-30 17:28:41 +0900

    testing release build change

--- src/material/tooltip/tooltip.ts
+++ src/material/tooltip/tooltip.ts
@@ -310,6 +310,7 @@ export class MatTooltip implements OnDestroy, AfterViewInit {
   /** The message to be displayed in the tooltip */
   @Input('matTooltip')
   get message(): string {
+    console.log('Test Output');
     return this._message;
   }
 
