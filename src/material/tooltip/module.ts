a227275791879a86efcaeb8ac1aba02685129537
Yosuke Yamashita <yosuke@aka.jp>
2025-06-05 22:17:23 +0900

    remove unused imports

--- src/material/tooltip/module.ts
+++ src/material/tooltip/module.ts
@@ -15,11 +15,9 @@ import {
 import {A11yModule} from '@angular/cdk/a11y';
 import {CdkScrollableModule} from '@angular/cdk/scrolling';
 import {CommonModule} from '@angular/common';
-import {DomSanitizer} from '@angular/platform-browser';
 import {MatCommonModule} from '@angular/material/core';
 import {NgModule} from '@angular/core';
 import {OverlayModule} from '@angular/cdk/overlay';
-import {TooltipCustomSanitizer} from './tooltip-custom-sanitizer';
 
 @NgModule({
   imports: [A11yModule, CommonModule, OverlayModule, MatCommonModule, MatTooltip, TooltipComponent],
